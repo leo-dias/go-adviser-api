17b4daebf365bcae636e4a00ee14d45a557e748b
Leonardo <leonardo.dias.oliveira@gmail.com>
2018-01-13 20:52:13 -0500

    Remove console.log from callback-controller

--- controllers/callback-controller.js
+++ controllers/callback-controller.js
@@ -4,6 +4,6 @@ module.exports = (err, res, result) => {
     if (err) return res.status(400).send(err);
 
     if (result.length === 0) return res.status(404).send();
-console.log('>>>>>>>>>>> Callback', result);
+
     return res.send(result);
 }
