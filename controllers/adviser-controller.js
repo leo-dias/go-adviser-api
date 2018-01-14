c25559445dc22c873aad7f41ab3640ba20b10a05
Leonardo <leonardo.dias.oliveira@gmail.com>
2018-01-13 20:32:17 -0500

    Accepted DEV modifications

--- controllers/adviser-controller.js
+++ controllers/adviser-controller.js
@@ -12,27 +12,10 @@ const controller = {
     }
     ,
     findByPriceVideo: (req, res) => {
-<<<<<<< HEAD
-        const price_init = req.query.init;
-        const price_end = req.query.end;
-
-        if (price_init === undefined || price_end === undefined) {
-            res.status(400).send({message: 'The parameters [init] and [end] are required.'});
-            return;
-        }
-
-        Adviser.find({}, fields)        
-            .where('price_consulting_video')
-            .gte(price_init).lte(price_end)
-            .populate('resume.skills')
-            .exec((err, result) => callback(err, res, result));
-    },
-=======
         if (req.query.init === undefined || req.query.end === undefined) {
             res.status(400).send({ message: 'The parameters [init] and [end] are required.' });
             return;
         }
->>>>>>> dev
 
         adviserRepository
             .findByPriceVideo(req.query.init, req.query.end)
@@ -41,32 +24,19 @@ const controller = {
     ,
     findBySkills: (req, res) => {
         if (!req.headers.skills) {
-<<<<<<< HEAD
-            res.status(400).send({message: 'No variable skills was found in header request.'});
-            return;
-        }
-
-        let skillsParam = JSON.parse('[' + req.headers.skills + ']');
-=======
             res.status(400).send({ message: 'No variable skills was found in header request.' });
             return;
         }
 
         let skillsParam = JSON.parse("[" + req.headers.skills + "]");
->>>>>>> dev
         let skills = [];
         for (let i = 0; i < skillsParam.length; i++) {
             skills.push(new ObjectId(skillsParam[i]));
         }
 
-<<<<<<< HEAD
-        Adviser.find({ 'resume.skills': { '$in': skills } }, fields, (err, result) => callback(err, res, result))
-        .populate('resume.skills');
-=======
         adviserRepository
             .findBySkills(skills)
             .then((err, result) => callback(err, res, result));
->>>>>>> dev
     }
 };
 
